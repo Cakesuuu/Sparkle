392ca6e28e9f82b2c4bed0f22da1c390b0a533af
Cakesuuu <cydrexvitug7220@gmail.com>
2024-06-10 00:26:22 +0800

    Try deploying to Github Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { easeIn, easeInOut, motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -10,22 +10,30 @@ export default function Home() {
 
         <div className="flex w-full min-h-screen justify-center items-center p-5">
 
-          <div className="flex flex-col h-full w-3/12 bg-[#c7172f] justify-center items-center rounded-[50px] rounded-br-none rounded-tr-none">
+          <div className="flex flex-col h-full w-3/12 bg-white justify-center items-center">
 
-            <motion.div className="flex flex-col h-fit w-full items-center p-5 gap-3 rounded-[50px] rounded-bl-none rounded-tl-none bg-white"
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-            >
+            <div className="h-full w-full bg-[#c7172f] rounded-[50px] rounded-br-none rounded-tr-none">
 
-              <h1 className="font-bento text-black text-7xl">Sparkle</h1>
+            </div>
 
-              <div className="border border-[#c7172f] w-11/12 rounded"></div>
+            <div className="bg-gradient-to-r from-white from-0% to-[#c7172f] to-40%">
 
-              <p className="text-black font-medium text-justify w-full h-fit p-5">
-                A member of the Masked Fools. Inscrutable and unscrupulous. A dangerous master of theatrics engrossed in playing roles. A woman of countless masks and many faces. Wealth, status, power... None of this matters to Sparkle. The only thing that can lure her interest is &apos;amusement.&apos;
-              </p>
+              <div className="flex flex-col h-fit w-full items-center p-5 gap-3 rounded-[50px] rounded-bl-none rounded-tl-none bg-white">
+                <h1 className="font-bento text-black text-7xl">Sparkle</h1>
 
-            </motion.div>
+                <div className="border border-[#c7172f] w-11/12 rounded"></div>
+
+                <p className="text-black font-medium text-justify w-full h-fit p-5">
+                  A member of the Masked Fools. Inscrutable and unscrupulous. A dangerous master of theatrics engrossed in playing roles. A woman of countless masks and many faces. Wealth, status, power... None of this matters to Sparkle. The only thing that can lure her interest is &apos;amusement.&apos;
+                </p>
+
+              </div>
+
+            </div>
+
+            <div className="h-full w-full bg-[#c7172f] rounded-[50px] rounded-br-none rounded-tr-none">
+
+            </div>
 
           </div>
 

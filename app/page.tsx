cb7b0476e34d3752b06c59cf3f86e079885741a5
Cakesuuu <cydrexvitug7220@gmail.com>
2024-06-10 03:36:21 +0800

    Added more stuff. Site is coming together.

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,27 @@ export default function Home() {
   return (
     <main className="bg-white">
 
-      <div className="flex min-h-screen w-full bg-white">
+      <div className="flex md:min-h-screen w-full bg-white">
 
-        <div className="flex flex-col-reverse md:flex-row w-full min-h-screen justify-center items-center p-5">
+        <div className="flex flex-col-reverse md:flex-row w-full md:min-h-screen justify-center items-center p-5">
 
-          <div className="flex flex-row md:flex-col h-fit md:h-full w-full md:w-4/12 justify-center items-center">
+          <div className="flex flex-row md:flex-col h-fit md:h-full w-full md:w-3/12 justify-center items-center">
 
             <div className="hidden md:block h-full w-full bg-[#c7172f] rounded-l-[50px]">
             </div>
 
-            <div className="bg-gradient-to-r from-white md:from-0% to-[#c7172f] to-40% rounded-[50px] md:rounded-none md:pr-10">
+            <div className="bg-gradient-to-r from-white md:from-0% to-[#c7172f] to-40% rounded-[50px] md:rounded-none">
 
               <div className="flex flex-col h-fit w-full items-center p-5 gap-3 rounded-[50px] md:rounded-bl-none md:rounded-tl-none bg-white">
+
                 <h1 className="font-bento text-black text-7xl">Sparkle</h1>
 
                 <div className="border border-[#c7172f] w-11/12 rounded"></div>
 
-                <p className="text-black font-medium w-full h-fit p-5">
-                  A member of the Masked Fools. Inscrutable and unscrupulous. A dangerous master of theatrics engrossed in playing roles. A woman of countless masks and many faces. Wealth, status, power... None of this matters to Sparkle. The only thing that can lure her interest is &apos;amusement.&apos;
+                <p className="text-black font-medium w-full text-justify h-fit p-5">
+                  A member of the Masked Fools. Inscrutable and unscrupulous. A dangerous master of theatrics engrossed in
+                  playing roles. A woman of countless masks and many faces. Wealth, status, power... None of this matters to
+                  Sparkle. The only thing that can lure her interest is &apos;amusement.&apos;
                 </p>
 
               </div>
@@ -36,24 +39,24 @@ export default function Home() {
 
           </div>
 
-          <div className="flex w-full md:w-8/12 h-full">
+          <div className="flex w-full md:w-9/12 h-fit md:h-full">
 
-            <div className="relative w-full h-auto md:bg-[#c7172f] rounded-[20px] md:rounded-l-none">
+            <div className="flex w-full h-auto md:bg-[#c7172f] rounded-[20px] md:rounded-l-none items-center justify-center">
 
-              <div className="absolute hidden md:flex w-full h-full items-center justify-center">
+              <div className="hidden absolute md:flex w-fit h-fit items-center justify-center">
 
-                <p className="font-ungai w-fit h-fit text-[5rem] md:text-[12rem] font-bold md:text-white">Sparkle</p>
+                <p className="font-hon w-fit h-fit text-[16rem] font-bold text-[#c7172f] md:text-white">Sparkle</p>
 
               </div>
 
-              <Image
-                src="./sparkle.png"
-                className="w-auto h-auto"
-                fill
-                unoptimized
-                alt=""
-                style={{ objectFit: "contain" }}
-              />
+              <div className="relative flex w-10/12 md:w-full md:h-[600px]">
+
+                <img
+                  src="./sparkle.png"
+                  className="w-full h-full object-contain max-w-full max-h-full"
+                />
+
+              </div>
 
             </div>
 
@@ -63,11 +66,39 @@ export default function Home() {
 
       </div>
 
-      <div className="flex min-h-screen w-full bg-white border-2 border-amber-300 p-5">
+      <div className="flex flex-col h-fit w-full items-center bg-white p-5 gap-5">
 
-        <div className="flex flex-col w-full border-2 border-black">
+        <h1 className="font-bento text-5xl md:text-6xl text-black">The Sparkling Splendor</h1>
+
+        <div className="relative flex w-full md:w-1/2 md:h-fit">
+
+          <img
+            src="./sparklebanner.jpg"
+            className="w-fit h-fit object-contain rounded-2xl"
+          />
 
         </div>
+
+        <p className="w-11/12 md:w-8/12 text-black text-sm md:text-base font-medium text-justify">
+          The girl, an abandoned orphan, lived without knowledge of her origins or destination until she encountered a passing troupe.
+          From a distance, she watched a black-haired lady with a double ponytail, gracefully moving across the stage like a fish.
+          Despite donning various masks, they did not hinder her laughs and cries during her performance, captivating the audience
+          even from afar. The fish leaped in front of her, then dove back down, creating ripples in the surface of the water.
+        </p>
+
+        <p className="w-11/12 md:w-8/12 text-black text-sm md:text-base font-medium text-justify">
+          Gradually, she realized she was watching the performance from beneath the stage.
+        </p>
+
+        <p className="w-11/12 md:w-8/12 text-black text-sm md:text-base font-medium text-justify">
+          Intrigued, she continued to watch the performances for countless days, but she remained an audience member,
+          never stepping into the spotlight. After one of the shows ended, she went backstage, where the black-haired
+          lady handed her a mask.
+        </p>
+
+        <p className="w-11/12 md:w-8/12 text-black text-sm md:text-base font-medium text-justify">
+          <i>"Can I perform, too?"</i>
+        </p>
 
       </div>
 

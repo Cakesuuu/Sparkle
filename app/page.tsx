188501468009774c3855490ff0fcdbf0cd6c85b1
Cakesuuu <cydrexvitug7220@gmail.com>
2024-06-10 02:26:02 +0800

    Fixed layout, added more stuff at the bottom.

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,23 @@ export default function Home() {
   return (
     <main className="bg-white">
 
-      <div className="flex min-h-screen w-full">
+      <div className="flex min-h-screen w-full bg-white">
 
-        <div className="flex w-full min-h-screen justify-center items-center p-5">
+        <div className="flex flex-col-reverse md:flex-row w-full min-h-screen justify-center items-center p-5">
 
-          <div className="flex flex-col h-full w-3/12 bg-white justify-center items-center">
+          <div className="flex flex-row md:flex-col h-fit md:h-full w-full md:w-4/12 justify-center items-center">
 
-            <div className="h-full w-full bg-[#c7172f] rounded-[50px] rounded-br-none rounded-tr-none">
-
+            <div className="hidden md:block h-full w-full bg-[#c7172f] rounded-l-[50px]">
             </div>
 
-            <div className="bg-gradient-to-r from-white from-0% to-[#c7172f] to-40%">
+            <div className="bg-gradient-to-r from-white md:from-0% to-[#c7172f] to-40% rounded-[50px] md:rounded-none md:pr-10">
 
-              <div className="flex flex-col h-fit w-full items-center p-5 gap-3 rounded-[50px] rounded-bl-none rounded-tl-none bg-white">
+              <div className="flex flex-col h-fit w-full items-center p-5 gap-3 rounded-[50px] md:rounded-bl-none md:rounded-tl-none bg-white">
                 <h1 className="font-bento text-black text-7xl">Sparkle</h1>
 
                 <div className="border border-[#c7172f] w-11/12 rounded"></div>
 
-                <p className="text-black font-medium text-justify w-full h-fit p-5">
+                <p className="text-black font-medium w-full h-fit p-5">
                   A member of the Masked Fools. Inscrutable and unscrupulous. A dangerous master of theatrics engrossed in playing roles. A woman of countless masks and many faces. Wealth, status, power... None of this matters to Sparkle. The only thing that can lure her interest is &apos;amusement.&apos;
                 </p>
 
@@ -32,19 +31,18 @@ export default function Home() {
 
             </div>
 
-            <div className="h-full w-full bg-[#c7172f] rounded-[50px] rounded-br-none rounded-tr-none">
-
+            <div className="hidden md:block h-full w-full bg-[#c7172f] rounded-l-[50px]">
             </div>
 
           </div>
 
-          <div className="flex w-9/12 h-full">
+          <div className="flex w-full md:w-8/12 h-full">
 
-            <div className="relative w-full h-full bg-[#c7172f] rounded-[50px] rounded-tl-none rounded-bl-none">
+            <div className="relative w-full h-auto md:bg-[#c7172f] rounded-[20px] md:rounded-l-none">
 
-              <div className="absolute flex w-full h-full items-center justify-center">
+              <div className="absolute hidden md:flex w-full h-full items-center justify-center">
 
-                <p className="font-ungai w-fit h-fit text-[12rem] font-bold text-white">Sparkle</p>
+                <p className="font-ungai w-fit h-fit text-[5rem] md:text-[12rem] font-bold md:text-white">Sparkle</p>
 
               </div>
 
@@ -60,6 +58,14 @@ export default function Home() {
             </div>
 
           </div>
+
+        </div>
+
+      </div>
+
+      <div className="flex min-h-screen w-full bg-white border-2 border-amber-300 p-5">
+
+        <div className="flex flex-col w-full border-2 border-black">
 
         </div>
 
